71a21e949d2a242ed65ff26236572f3ed19bbb90
gunjanvishwakarma <gunjan.it2k4@gmail.com>
2019-05-31 10:04:25 +0530

    Configuration

---
+++ limits-service/src/main/java/com/gunjan/microservices/limitsservice/LimitConfiguration.java
@@ -0,0 +1,37 @@
+package com.gunjan.microservices.limitsservice;
+
+public class LimitConfiguration
+{
+    int minimum;
+    int maximum;
+    
+    public LimitConfiguration(){
+    
+    }
+    
+    public LimitConfiguration(int minimum, int maximum)
+    {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+    
+    public int getMinimum()
+    {
+        return minimum;
+    }
+    
+    public void setMinimum(int minimum)
+    {
+        this.minimum = minimum;
+    }
+    
+    public int getMaximum()
+    {
+        return maximum;
+    }
+    
+    public void setMaximum(int maximum)
+    {
+        this.maximum = maximum;
+    }
+}

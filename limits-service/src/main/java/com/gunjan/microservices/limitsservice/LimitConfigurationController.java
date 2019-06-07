cc3bb72bbc582012280fddf464d05d52aee37916
gunjanvishwakarma <gunjan.it2k4@gmail.com>
2019-06-07 14:27:21 +0530

    spring cloud

--- limits-service/src/main/java/com/gunjan/microservices/limitsservice/LimitConfigurationController.java
+++ limits-service/src/main/java/com/gunjan/microservices/limitsservice/LimitConfigurationController.java
@@ -1,5 +1,6 @@
 package com.gunjan.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,15 @@ public class LimitConfigurationController
     @GetMapping("/limits")
     public LimitConfiguration reteriveLimitConfigurations(){
         return new LimitConfiguration(configuration.getMinimum(),configuration.getMaximum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration reteriveConfigurations(){
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration(){
+        return new LimitConfiguration(10,100);
     }
 }
